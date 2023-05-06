590a52ef0977930f32ef2c47f63e7a5d30440a78
Lukibwoy <lukaszwiczynski@gmail.com>
2023-05-06 10:58:58 +0200

    NegToPositiveNumber

--- js/script.js
+++ js/script.js
@@ -174,3 +174,25 @@ function century(year) {
 	  }
 	  
 	  
+// 	  Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+
+
+// by map
+
+function invert(array) {
+	return array.map((number) => number * -1)
+  }
+
+// by for
+
+function invert(array) {
+	var newArr = []
+	for(let i =0; i<array.length;i++){
+	  newArr.push(-array[i])
+	}
+	  return newArr
+	}
