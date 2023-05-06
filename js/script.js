2f1f6177987529ff6e3ccd36576dbf92f5b3b216
Lukibwoy <lukaszwiczynski@gmail.com>
2023-05-06 10:25:12 +0200

    Counting sheep

--- js/script.js
+++ js/script.js
@@ -159,3 +159,18 @@ function century(year) {
 		  return number*9
 		}
 	}
+
+// 	Counting sheep...
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+	function countSheeps(arrayOfSheep) {
+		let Sheep = 0
+		for(let i=0; i<arrayOfSheep.length; i++){
+		  if (arrayOfSheep[i] === true){
+			Sheep++;
+		  }
+		}
+		return Sheep;
+	  }
+	  
+	  
