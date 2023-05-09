24300b403617761051db82e9ec3a6929f66d4e65
Lukibwoy <lukaszwiczynski@gmail.com>
2023-05-09 19:13:29 +0200

    remove smallest

---
+++ js/basics_tasks/stringConv.js
@@ -0,0 +1,11 @@
+//   Convert a string to an array
+
+//   Write a function to split a string and convert it into an array of words.
+//   Examples (Input ==> Output):
+
+//   "Robin Singh" ==> ["Robin", "Singh"]
+
+function stringToArray(string) {
+	const newArr = string.split(' ')
+	return newArr
+}
